7a686855542856116b38a61ba8c714d0a66ac46d
Navezm <navez.martin@gmail.com>
2021-01-19 10:19:08 +0100

    Exo terminé

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, {useState} from 'react';
+import Dossier from './Component';
 
 let App = () => {
   const [contenu] = useState("La CS15 best coding ever !")
+  const [titre] = useState("Died of boredom")
   return (
     <div className="App">
       <h1>{contenu}</h1>
+      <Dossier name={titre}/>
     </div>
   );
 }
